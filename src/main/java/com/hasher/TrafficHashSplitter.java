a22c10b575f33b7370f17e6bc3f2a9419571e157
Tahir Wasim <tahir.wasim@groupbyinc.com>
2016-12-28 16:18:51 -0500

    add murmur seed, pass expected buckets test

--- src/main/java/com/hasher/TrafficHashSplitter.java
+++ src/main/java/com/hasher/TrafficHashSplitter.java
@@ -4,13 +4,9 @@ import com.sangupta.murmur.Murmur3;
 
 import java.util.stream.IntStream;
 
-/**
- * TrafficHashSplitter
- *
- * @author groupby
- */
 public class TrafficHashSplitter {
 
+    private static final long MURMUR_SEED = 2321168210L;
     private static final long MAX_HASH_VALUE = (long) Math.floor(Math.pow(2, 32));
 
     public static int getBucketFromSessionId(String sessionId, int trafficAllocation, int[] bucketPercentages) {
@@ -19,8 +15,7 @@ public class TrafficHashSplitter {
 
         long[] bucketMaxHashValues = mapFractionsToThresholds(trafficAllocation, fractions);
 
-
-        long sessionHash = Murmur3.hash_x86_32();
+        long sessionHash = Murmur3.hash_x86_32(sessionId.getBytes(), sessionId.length(), MURMUR_SEED);
 
         int bucket = 0;
 
@@ -31,19 +26,24 @@ public class TrafficHashSplitter {
                 return bucket;
             }
         }
+
         return -1;
     }
 
     public static long[] mapFractionsToThresholds(int trafficAllocation, double[] fractions) {
+
         long[] bucketMaxHashValues = new long[fractions.length];
+
         for (int index = 0; index < fractions.length; index++) {
             long maxHashValue = (long) Math.floor(fractions[index] * trafficAllocation / 100 * MAX_HASH_VALUE);
             bucketMaxHashValues[index] = maxHashValue;
         }
+
         return bucketMaxHashValues;
     }
 
     public static double[] generateBucketFractions(int[] bucketPercentages) {
+
         int sum = IntStream.of(bucketPercentages).sum();
 
         if (sum != 100) {
@@ -51,10 +51,12 @@ public class TrafficHashSplitter {
         }
 
         double[] fractions = new double[bucketPercentages.length];
+
         for (int index = 0; index < bucketPercentages.length; index++) {
             fractions[index] = index > 0 ? (double) bucketPercentages[index] / 100 + fractions[index - 1]
                                          : (double) bucketPercentages[index] / 100;
         }
+
         return fractions;
     }
 }
--- src/test/java/com/hasher/TrafficHashSplitterTest.java
+++ src/test/java/com/hasher/TrafficHashSplitterTest.java
@@ -3,17 +3,12 @@ package com.hasher;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Scanner;
 
 import static com.hasher.TrafficHashSplitter.generateBucketFractions;
-import static com.hasher.TrafficHashSplitter.getBucketFromSessionId;
 import static com.hasher.TrafficHashSplitter.mapFractionsToThresholds;
 import static org.junit.Assert.assertArrayEquals;
 
-/**
- * @author groupby
- */
 public class TrafficHashSplitterTest {
     private static final int TRAFFIC_ALLOCATION = 75;
     private static final int[] BUCKET_PERCENTAGES = {25, 30, 30, 15};
@@ -35,15 +30,8 @@ public class TrafficHashSplitterTest {
         for (int ind = 0; ind < testStrings.length; ind++) {
             expectedBuckets[ind] = Integer.parseInt(expBucketsStr[ind]);
         }
-        System.out.println(Arrays.toString(hashResults));
         assertArrayEquals(expectedBuckets, hashResults);
     }
-
-    @Test
-    public void random() throws Exception {
-        getBucketFromSessionId("akdjflakdfj", 75, new int[]{10,20,30,40});
-    }
-
 
     @Test
     public void testMapsFractionsToThresholds() throws Exception {
@@ -60,7 +48,7 @@ public class TrafficHashSplitterTest {
     @Test
     public void testGenerateBucketsFractionsReturnsAsExpected() throws Exception {
         int[] testBuckets = {10, 20, 30, 40};
-        assertArrayEquals(new double[]{0.1, 0.3, 0.6, 1}, TrafficHashSplitter.generateBucketFractions(testBuckets), 0.001);
+        assertArrayEquals(
+                new double[]{0.1, 0.3, 0.6, 1}, TrafficHashSplitter.generateBucketFractions(testBuckets), 0.001);
     }
-
 }
