155f4c7689d26360d5bc92b9867dcd84f50406fc
Will Warren <will.warren@groupbyinc.com>
2016-12-30 10:40:48 -0500

    todo

--- src/main/java/com/groupbyinc/example/Driver.java
+++ src/main/java/com/groupbyinc/example/Driver.java
@@ -12,7 +12,7 @@ public class Driver {
   public static void main(String[] args) throws IOException {
     URL url = Resources.getResource(args[0]);
     String bucketConfigurationContents = Resources.toString(url, Charsets.UTF_8);
-    BucketConfiguration bucketConfiguration = new BucketConfiguration();
+    BucketConfiguration bucketConfiguration = new BucketConfiguration();  // Use Jackson to do this.
     String sessionId = args[1];
     int bucket = TrafficHashSplitter.getBucketFromSessionId(sessionId, bucketConfiguration);
     System.out.println("Bucket: " + bucket);
